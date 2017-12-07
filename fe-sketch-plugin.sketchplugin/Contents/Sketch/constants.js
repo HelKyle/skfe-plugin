7f12a4cc6c6ef5fae0ff24e18d7eb27a79d4525d
zhouxiaokai <helkylefighting@gmail.com>
2017-12-07 21:26:19 +0800

    init

---
+++ fe-sketch-plugin.sketchplugin/Contents/Sketch/constants.js
@@ -0,0 +1,11 @@
+var TEXT_ALIGNMENT = {
+  LEFT: 0,
+  RIGHT: 1,
+  CENTER: 2
+}
+
+var TEXT_ALIGNMENT_MAP = {
+  LEFT: 'left',
+  RIGHT: 'right',
+  CENTER: 'center'
+}
---
+++ fe-sketch-plugin.sketchplugin/Contents/Sketch/plugin.js
@@ -0,0 +1,124 @@
+@import 'constants.js'
+var identifier = "com.helkyle.panel";
+
+function toArray (object) {
+  if (Array.isArray(object)) {
+    return object;
+  }
+  let arr = [];
+  for (let j = 0; j < object.count(); j++) {
+    arr.push(object.objectAtIndex(j));
+  }
+  return arr;
+}
+
+function togglePanel (context) {
+  if (isOpen()) {
+    close(context);
+  } else {
+    open(context);
+  }
+}
+
+function isOpen () {
+  var threadDictionary = NSThread.mainThread().threadDictionary();
+  return threadDictionary[identifier];
+}
+
+function close (context) {
+  var threadDictionary = NSThread.mainThread().threadDictionary();
+  threadDictionary.removeObjectForKey(identifier);
+  const contentView = context.document.documentWindow().contentView();
+  if (!contentView) {
+    return false;
+  }
+  const stageView = contentView.subviews().objectAtIndex(0);
+  const finalViews = toArray(stageView.subviews()).filter(view => {
+    return view.identifier() != identifier;
+  });
+  stageView.subviews = finalViews;
+  stageView.adjustSubviews();
+}
+
+function open (context) {
+  var panelWidth = 340;
+  var panelHeight = 646;
+
+  var threadDictionary = NSThread.mainThread().threadDictionary();
+  if (threadDictionary[identifier]) return;
+
+  var webView = WebView.alloc().initWithFrame(NSMakeRect(0, 0, panelWidth, panelHeight - 44));
+  var request = NSURLRequest.requestWithURL(context.plugin.urlForResourceNamed("webView.html"));
+  webView.mainFrame().loadRequest(request);
+  webView.setDrawsBackground(false);
+  webView.identifier = identifier;
+
+  const contentView = context.document.documentWindow().contentView();
+  const stageView = contentView.subviews().objectAtIndex(0);
+  const views = stageView.subviews();
+  let finalViews = [];
+  let pushedWebView = false;
+  for (let i = 0; i < views.count(); i++) {
+    const view = views.objectAtIndex(i);
+    finalViews.push(view);
+    if (!pushedWebView && view.identifier() == 'view_canvas') {
+      finalViews.push(webView);
+      pushedWebView = true;
+    }
+  }
+  if (!pushedWebView) {
+    finalViews.push(webView);
+  }
+  stageView.subviews = finalViews;
+  stageView.adjustSubviews();
+  threadDictionary[identifier] = webView;
+}
+
+var onSelectionChanged = function(context) {
+  var threadDictionary = NSThread.mainThread().threadDictionary();
+  
+  if (threadDictionary[identifier]) {
+    var webView = threadDictionary[identifier];
+    if (!webView) {
+      return;
+    }
+    var windowObject = webView.windowScriptObject();
+
+    var selection = context.actionContext.document.selectedLayers().layers();
+    if (selection.length >= 1) {
+      let layer = selection[0];
+      var frame = layer.frame();
+      var attributes = layer.CSSAttributes();
+
+      if (layer.class() == "MSTextLayer") {
+        if (!layer.lineHeight()) {
+          attributes.addObject(`line-height: 1.4;`);
+        }
+        for (const key in TEXT_ALIGNMENT) {
+          if (layer.textAlignment() === TEXT_ALIGNMENT[key]) {
+            attributes.addObject(`text-align: ${TEXT_ALIGNMENT_MAP[key]};`);
+          }
+        }
+      }
+      attributes.addObject('\n');
+      attributes.addObject('/* Size: */');
+      attributes.addObject(`width: ${frame.width()}px;`);
+      attributes.addObject(`height: ${frame.height()}px;`);
+      let params = {}
+      if (layer.class() == "MSTextLayer") {
+        params = {
+          attributes: attributes.join(" "),
+          content: String(layer.stringValue())
+        }
+      } else {
+        params = {
+          attributes: attributes.join(" "),
+          content: ""
+        }
+      }
+      windowObject.evaluateWebScript(`updatePreview(${JSON.stringify(params)})`);
+    } else {
+      windowObject.evaluateWebScript("updatePreview(' ')");
+    }
+  }
+};
